e4f2631fc51fbb47f4f084c4a541944975e8b0dd
cfrancesco <testadicardi@gmail.com>
2020-10-19 17:54:00 +0200

    Removed normalizer choice.

--- dfa/audio.py
+++ dfa/audio.py
@@ -1,15 +1,5 @@
-import sys
-
 import librosa
 import numpy as np
-
-
-class Normalizer:
-    def normalize(self, S):
-        raise NotImplementedError
-    
-    def denormalize(self, S):
-        raise NotImplementedError
 
 
 class Audio:
@@ -22,8 +12,7 @@ class Audio:
                  win_length: int,
                  n_filters: int,
                  fmin: int,
-                 fmax: int,
-                 normalizer: Normalizer):
+                 fmax: int):
         self.n_mels = n_mels
         self.sample_rate = sample_rate
         self.hop_length = hop_length
@@ -31,7 +20,6 @@ class Audio:
         self.n_filters = n_filters
         self.fmin = fmin
         self.fmax = fmax
-        self.normalizer = normalizer
     
     def load_wav(self, path):
         wav, _ = librosa.load(path, sr=self.sample_rate)
@@ -50,15 +38,17 @@ class Audio:
         mel = mel.T
         return self.normalize(mel)
     
-    def normalize(self, mel):
-        return self.normalizer.normalize(mel)
+    @staticmethod
+    def normalize(mel):
+        S = np.clip(mel, a_min=1.0e-5, a_max=None)
+        return np.log(S)
     
-    def denormalize(self, mel):
-        return self.normalizer.denormalize(mel)
+    @staticmethod
+    def denormalize(mel):
+        return np.exp(mel)
     
     @classmethod
     def from_config(cls, config):
-        normalizer = getattr(sys.modules[__name__], config['normalizer'])()
         return cls(
             sample_rate=config['sample_rate'],
             n_filters=config['n_filters'],
@@ -66,41 +56,4 @@ class Audio:
             win_length=config['win_length'],
             hop_length=config['hop_length'],
             fmin=config['fmin'],
-            fmax=config['fmax'],
-            normalizer=normalizer)
-
-
-class MelGAN(Normalizer):
-    def __init__(self):
-        super().__init__()
-        self.clip_min = 1.0e-5
-    
-    def normalize(self, S):
-        S = np.clip(S, a_min=self.clip_min, a_max=None)
-        return np.log(S)
-    
-    def denormalize(self, S):
-        return np.exp(S)
-
-
-class WaveRNN(Normalizer):
-    def __init__(self):
-        super().__init__()
-        self.min_level_db = - 100
-        self.max_norm = 4
-    
-    def normalize(self, S):
-        S = self.amp_to_db(S)
-        S = np.clip((S - self.min_level_db) / -self.min_level_db, 0, 1)
-        return (S * 2 * self.max_norm) - self.max_norm
-    
-    def denormalize(self, S):
-        S = (S + self.max_norm) / (2 * self.max_norm)
-        S = (np.clip(S, 0, 1) * -self.min_level_db) + self.min_level_db
-        return self.db_to_amp(S)
-    
-    def amp_to_db(self, x):
-        return 20 * np.log10(np.maximum(1e-5, x))
-    
-    def db_to_amp(self, x):
-        return np.power(10.0, x * 0.05)
+            fmax=config['fmax'])
