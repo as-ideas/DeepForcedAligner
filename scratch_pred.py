1bf11aba7b95afe6cd182d42c7037fc6f4536960
cfrancesco <testadicardi@gmail.com>
2020-10-22 14:57:55 +0200

    Prediction is mostly from config.

--- scratch_pred.py
+++ scratch_pred.py
@@ -1,4 +1,4 @@
-from pathlib import Path
+import argparse
 
 import numpy as np
 import torch
@@ -8,45 +8,46 @@ from dfa.duration_extraction import extract_durations_with_dijkstra
 from dfa.model import Aligner
 from dfa.text import Tokenizer
 from dfa.utils import read_metafile
+from dfa.utils import read_config
+from dfa.paths import Paths
 
 if __name__ == '__main__':
-
-    checkpoint = torch.load('/Users/cschaefe/dfa_checkpoints/latest_model_old.pt', map_location=torch.device('cpu'))
+    parser = argparse.ArgumentParser(description='Preprocessing for DeepForcedAligner.')
+    parser.add_argument('--config', '-c', default='config.yaml', help='Points to the config file.')
+    args = parser.parse_args()
+    config = read_config(args.config)
+    paths = Paths.from_config(config['paths'])
+    checkpoint = torch.load('/Volumes/data/logs/dfa/latest_model.pt', map_location=torch.device('cpu'))
     config = checkpoint['config']
     symbols = checkpoint['symbols']
     audio = Audio.from_config(config['audio'])
     tokenizer = Tokenizer(symbols)
     model = Aligner.from_checkpoint(checkpoint).eval()
     print(f'model step {model.get_step()}')
-
-    main_dir = Path('/Users/cschaefe/datasets/audio_data/Cutted_merged')
-    text_dict = read_metafile(main_dir)
-    file_id = '04902'
-    wav = audio.load_wav(main_dir / f'{file_id}.wav')
+    
+    main_dir = paths.dataset_dir
+    text_dict = read_metafile(paths.metadata_path)
+    file_id = list(text_dict.keys())[0]
     text = text_dict[file_id]
-
+    
     target = np.array(tokenizer(text))
-
-    mel = audio.wav_to_mel(wav)
+    
+    mel = np.load((paths.mel_dir / file_id).with_suffix('.npy'))
     mel = torch.tensor(mel).float().unsqueeze(0)
-
+    
     pred = model(mel)
-
+    
     pred_max = pred[0].max(1)[1].detach().cpu().numpy().tolist()
     pred_text = tokenizer.decode(pred_max)
-
     pred = torch.softmax(pred, dim=-1)
     pred = pred.detach()[0].numpy()
-
+    
     target_len = target.shape[0]
     pred_len = pred.shape[0]
-
+    
     durations = extract_durations_with_dijkstra(target, pred)
     expanded_string = ''.join([text[i] * dur for i, dur in enumerate(list(durations))])
     print(text)
     print(pred_text)
     print(expanded_string)
     print(durations)
-
-
-
