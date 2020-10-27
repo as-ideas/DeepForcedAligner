7de948406c73bbfe62bf5a40dc3b46c89c488060
cschaefer26 <c.schaefer.home@gmail.com>
2020-10-27 13:20:29 +0100

    Optional transposing of mels in preprocessing.

--- preprocess.py
+++ preprocess.py
@@ -17,20 +17,25 @@ class Preprocessor:
     """Performs mel extraction and tokenization and stores the resulting torch tensors."""
     
     def __init__(self, audio: Audio, tokenizer: Tokenizer,
-                 paths: Paths, text_dict: Dict[str, str]) -> None:
+                 paths: Paths, text_dict: Dict[str, str],
+                 mel_dim_last=True) -> None:
         self.audio = audio
         self.paths = paths
         self.tokenizer = tokenizer
         self.text_dict = text_dict
-        self.compute_mels = paths.precomputed_mels is None
+        self.mel_dim_last = mel_dim_last
     
     def __call__(self, wav_path: Path) -> Dict[str, Union[str, int]]:
         item_id = wav_path.stem
-        if self.compute_mels:
+        if self.paths.precomputed_mels:
+            mel = np.load(self.paths.precomputed_mels / f'{item_id}.npy')
+            if not self.mel_dim_last:
+                mel = mel.T
+                print('b')
+        else:
             wav = self.audio.load_wav(wav_path)
             mel = self.audio.wav_to_mel(wav)
-        else:
-            mel = np.load(self.paths.precomputed_mels / f'{item_id}.npy')
+
         np.save(self.paths.mel_dir / f'{item_id}.npy', mel, allow_pickle=False)
         text = self.text_dict[item_id]
         tokens = np.array(self.tokenizer(text)).astype(np.int32)
@@ -41,10 +46,13 @@ class Preprocessor:
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Preprocessing for DeepForcedAligner.')
     parser.add_argument('--config', '-c', help='Points to the config file.', default='config.yaml')
+    parser.add_argument('--mel_dim_last', action='store_true',
+                        help='Set if precomputed mels have mel channels as last dimension.')
     parser.add_argument('--num_workers', '-w', metavar='N', type=int, default=cpu_count() - 1,
                         help='The number of worker threads to use for preprocessing')
+
     args = parser.parse_args()
-    
+
     config = read_config(args.config)
     paths = Paths.from_config(config['paths'])
     audio = Audio.from_config(config['audio'])
@@ -60,8 +68,8 @@ if __name__ == '__main__':
     wav_files = get_files(paths.dataset_dir, extension='.wav')
     wav_files = [x for x in wav_files if x.stem in text_dict] # for filtering in the metadata
     tokenizer = Tokenizer(symbols)
-    preprocessor = Preprocessor(audio=audio, tokenizer=tokenizer,
-                                paths=paths, text_dict=text_dict)
+    preprocessor = Preprocessor(audio=audio, tokenizer=tokenizer, paths=paths,
+                                text_dict=text_dict, mel_dim_last=args.mel_dim_last)
     pool = Pool(processes=args.num_workers)
     mapper = pool.imap_unordered(preprocessor, wav_files)
     dataset = []
