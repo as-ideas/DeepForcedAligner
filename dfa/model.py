322a017ef361efb17a966231bc969fad52c0e834
cschaefer26 <c.schaefer.home@gmail.com>
2020-11-09 10:40:49 +0100

    Trying taco style.

--- dfa/model.py
+++ dfa/model.py
@@ -1,6 +1,6 @@
 import torch
 import torch.nn as nn
-
+import torch.nn.functional as F
 
 class BatchNormConv(nn.Module):
 
@@ -61,6 +61,23 @@ class Aligner(torch.nn.Module):
         return model
 
 
+class PreNet(nn.Module):
+    def __init__(self, in_dims, fc1_dims=256, fc2_dims=128, dropout=0.5):
+        super().__init__()
+        self.fc1 = nn.Linear(in_dims, fc1_dims)
+        self.fc2 = nn.Linear(fc1_dims, fc2_dims)
+        self.p = dropout
+
+    def forward(self, x):
+        x = self.fc1(x)
+        x = F.relu(x)
+        x = F.dropout(x, self.p, training=True)
+        x = self.fc2(x)
+        x = F.relu(x)
+        x = F.dropout(x, self.p, training=True)
+        return x
+
+
 class TTSModel(torch.nn.Module):
 
     def __init__(self,
@@ -70,17 +87,18 @@ class TTSModel(torch.nn.Module):
                  conv_dim: int) -> None:
         super().__init__()
         self.register_buffer('step', torch.tensor(1, dtype=torch.int))
-        self.convs = nn.ModuleList([
-            BatchNormConv(num_symbols, conv_dim, 5),
-        ])
-        self.lin = torch.nn.Linear(conv_dim, n_mels)
+        self.prenet = PreNet(n_mels)
+        self.rnn = torch.nn.LSTM(num_symbols + 128, lstm_dim, batch_first=True, bidirectional=False)
+        self.lin = torch.nn.Linear(lstm_dim, n_mels)
         self.n_mels = n_mels
 
-    def forward(self, x):
+    def forward(self, x, mel):
         if self.train:
             self.step += 1
-        for conv in self.convs:
-            x = conv(x)
+        mel_in = torch.cat([torch.zeros(x.size(0), 1, self.n_mels), mel[:, :-1, :]], dim=1)
+        mel_in = self.prenet(mel_in)
+        x = torch.cat([x, mel_in], dim=-1)
+        x, _ = self.rnn(x)
         x = self.lin(x)
         return x
 
