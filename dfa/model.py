4c6ea333820b3b67780ec77f527b17d8891446ae
cschaefer26 <c.schaefer.home@gmail.com>
2020-11-09 11:43:55 +0100

    Trying lstm.

--- dfa/model.py
+++ dfa/model.py
@@ -1,6 +1,6 @@
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
+
 
 class BatchNormConv(nn.Module):
 
@@ -61,23 +61,6 @@ class Aligner(torch.nn.Module):
         return model
 
 
-class PreNet(nn.Module):
-    def __init__(self, in_dims, fc1_dims=256, fc2_dims=128, dropout=0.5):
-        super().__init__()
-        self.fc1 = nn.Linear(in_dims, fc1_dims)
-        self.fc2 = nn.Linear(fc1_dims, fc2_dims)
-        self.p = dropout
-
-    def forward(self, x):
-        x = self.fc1(x)
-        x = F.relu(x)
-        x = F.dropout(x, self.p, training=True)
-        x = self.fc2(x)
-        x = F.relu(x)
-        x = F.dropout(x, self.p, training=True)
-        return x
-
-
 class TTSModel(torch.nn.Module):
 
     def __init__(self,
@@ -87,19 +70,13 @@ class TTSModel(torch.nn.Module):
                  conv_dim: int) -> None:
         super().__init__()
         self.register_buffer('step', torch.tensor(1, dtype=torch.int))
-        self.prenet = PreNet(n_mels)
-        self.rnn = torch.nn.LSTM(num_symbols + 128, lstm_dim, batch_first=True, bidirectional=False)
-        self.lin = torch.nn.Linear(lstm_dim, n_mels)
+        self.rnn = torch.nn.LSTM(num_symbols, lstm_dim, batch_first=True, bidirectional=True)
+        self.lin = torch.nn.Linear(2*lstm_dim, n_mels)
         self.n_mels = n_mels
 
-    def forward(self, x, mel):
+    def forward(self, x):
         if self.train:
             self.step += 1
-
-        device = next(self.parameters()).device
-        mel_in = torch.cat([torch.zeros(x.size(0), 1, self.n_mels, device=device), mel[:, :-1, :]], dim=1)
-        mel_in = self.prenet(mel_in)
-        x = torch.cat([x, mel_in], dim=-1)
         x, _ = self.rnn(x)
         x = self.lin(x)
         return x
