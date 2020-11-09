f24f331f0a936352d54307b5e50056916e7e2129
cschaefer26 <c.schaefer.home@gmail.com>
2020-11-09 14:42:19 +0100

    Back to single lstm.

--- dfa/model.py
+++ dfa/model.py
@@ -73,20 +73,13 @@ class TTSModel(torch.nn.Module):
                  conv_dim: int) -> None:
         super().__init__()
         self.register_buffer('step', torch.tensor(1, dtype=torch.int))
-        self.convs = nn.ModuleList([
-            BatchNormConv(num_symbols, conv_dim, 5),
-            BatchNormConv(conv_dim, conv_dim, 5),
-            BatchNormConv(conv_dim, conv_dim, 5),
-        ])
-        self.rnn = torch.nn.LSTM(conv_dim, lstm_dim, batch_first=True, bidirectional=True)
+        self.rnn = torch.nn.LSTM(num_symbols, lstm_dim, batch_first=True, bidirectional=True)
         self.lin = torch.nn.Linear(2*lstm_dim, n_mels)
         self.n_mels = n_mels
 
     def forward(self, x):
         if self.train:
             self.step += 1
-        for conv in self.convs:
-            x = conv(x)
         x, _ = self.rnn(x)
         x = self.lin(x)
         return x
