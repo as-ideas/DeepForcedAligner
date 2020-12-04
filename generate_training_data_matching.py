fa7cf163aeef763b1348e9d2e1a9cb57d3f1a2c6
cschaefer26 <c.schaefer.home@gmail.com>
2020-12-04 18:30:28 +0100

    Using ends.

--- generate_training_data_matching.py
+++ generate_training_data_matching.py
@@ -19,6 +19,7 @@ class hp:
     min_voice_length = 4
     vad_sample_rate = 48000
 
+
 def trim_end(wav):
     int16_max = (2 ** 15) - 1
     samples_per_window = (hp.vad_window_length * hp.vad_sample_rate) // 1000
@@ -43,10 +44,37 @@ def trim_end(wav):
     audio_mask = np.invert(audio_mask)
     voice_indices = np.where(audio_mask)[0]
     voice_start, voice_end = voice_indices[0], voice_indices[-1]
-    audio_mask[:voice_end] = 1
+    audio_mask[:voice_end+hp.min_voice_length] = 1
     audio_mask = np.repeat(audio_mask, samples_per_window)
     return wav[audio_mask]
 
+
+def get_end(wav):
+    int16_max = (2 ** 15) - 1
+    samples_per_window = (hp.vad_window_length * hp.vad_sample_rate) // 1000
+    wav = wav[:len(wav) - (len(wav) % samples_per_window)]
+    pcm_wave = struct.pack("%dh" % len(wav), *(np.round(wav * int16_max)).astype(np.int16))
+    voice_flags = []
+    vad = webrtcvad.Vad(mode=3)
+    for window_start in range(0, len(wav), samples_per_window):
+        window_end = window_start + samples_per_window
+        voice_flags.append(vad.is_speech(pcm_wave[window_start * 2:window_end * 2],
+                                         sample_rate=hp.vad_sample_rate))
+    voice_flags = np.array(voice_flags)
+    def moving_average(array, width):
+        array_padded = np.concatenate((np.zeros((width - 1) // 2), array, np.zeros(width // 2)))
+        ret = np.cumsum(array_padded, dtype=float)
+        ret[width:] = ret[width:] - ret[:-width]
+        return ret[width - 1:] / width
+    audio_mask = moving_average(voice_flags, hp.vad_moving_average_width)
+    audio_mask = np.round(audio_mask).astype(np.bool)
+    audio_mask = np.invert(audio_mask)
+    audio_mask[:] = binary_dilation(audio_mask[:], np.ones(hp.min_voice_length + 1))
+    audio_mask = np.invert(audio_mask)
+    audio_mask = np.repeat(audio_mask, samples_per_window)
+    voice_indices = np.where(audio_mask)[0]
+    voice_start, voice_end = voice_indices[0], voice_indices[-1]
+    return voice_end
 
 class Preprocessor:
 
@@ -57,6 +85,7 @@ class Preprocessor:
         self.out_path = out_path
 
     def __call__(self, file_name):
+        scores = []
         try:
             print(f'Generating for dir {file_name}')
             wav_path = self.wav_main / file_name
@@ -67,7 +96,6 @@ class Preprocessor:
             starts = []
             start = 0
 
-            scores = []
             for i, wav in enumerate(wavs):
                 wav_snippet = self.audio.load_wav(wav)
                 window = min(len(wav_snippet), 50000)
@@ -82,27 +110,46 @@ class Preprocessor:
                         min_t = t
                     if min_diff < 15 and diff > min_diff + 10:
                         break
-                print(f'{file_name} {wav.stem} {min_diff} {min_t}')
+                print(f'{file_name} (start) {wav.stem} {min_diff} {min_t}')
                 starts.append((min_t, min_diff))
                 scores.append((wav.stem, min_diff))
                 start = min_t
 
+            ends = []
+            end = len(wav_long)
+            scores_ends = []
+            for i, wav in enumerate(reversed(wavs)):
+                wav_snippet = self.audio.load_wav(wav)
+                snippet_end = get_end(wav_snippet)
+                window = min(len(wav_snippet)-snippet_end, 50000)
+                stride = 100
+                wav_part = wav_snippet[snippet_end-window:snippet_end:stride]
+                min_diff = 9999999
+                min_t = end
+                for t in range(end, max(end - 2500000, window), -1):
+                    diff = np.sum(np.abs(wav_long[t - window:t:stride] - wav_part))
+                    if diff < min_diff:
+                        min_diff = diff
+                        min_t = t
+                print(f'{file_name} (end) {wav.stem} {min_diff} {min_t}')
+                ends.append((min_t, min_diff))
+                scores_ends.append((wav.stem, min_diff))
+                end = min_t
+
+            scores_ends.reverse()
+            ends.reverse()
+
             for i, wav in enumerate(wavs):
-                try:
-                    name = wav.stem
-                    start = starts[i][0]
-                    if i < len(wavs) - 1:
-                        end = starts[i + 1][0]
-                    else:
-                        end = len(wav_long)
-                    wav_cut = wav_long[start:end]
-                    wav_cut = trim_end(wav_cut)
-                    sf.write(self.out_path / f'{name}.wav', wav_cut, samplerate=self.audio.sample_rate)
-                except Exception as e:
-                    print(e)
+                name = wav.stem
+                start = starts[i][0]
+                end = ends[i][0]
+                wav_cut = wav_long[start:end]
+                wav_cut = trim_end(wav_cut)
+                sf.write(self.out_path / f'{name}.wav', wav_cut, samplerate=self.audio.sample_rate)
             return scores
         except Exception as e:
             print(e)
+            return scores_ends
 
 
 if __name__ == '__main__':
@@ -122,7 +169,7 @@ if __name__ == '__main__':
     preprocessor = Preprocessor(audio=audio, wav_main=wav_main,
                                 wav_long_main=wav_long_main, out_path=out_path)
 
-    pool = Pool(processes=8)
+    pool = Pool(processes=4)
     mapper = pool.imap_unordered(preprocessor, snippet_dirs)
     all_scores = []
 
