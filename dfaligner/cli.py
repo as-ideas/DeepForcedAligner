3e852c9b2044cf46709c6250be3ee821aea0cd64
Aidan Pine <hello@aidanpine.ca>
2022-11-04 21:58:58 +0000

    refactor(cli): factor out config override

--- dfaligner/cli.py
+++ dfaligner/cli.py
@@ -1,4 +1,3 @@
-import json
 import os
 from enum import Enum
 from pathlib import Path
@@ -7,13 +6,12 @@ from typing import List, Optional
 import numpy as np
 import torch
 import typer
-import yaml
 from loguru import logger
 from pytorch_lightning import Trainer
 from pytorch_lightning.callbacks import LearningRateMonitor, ModelCheckpoint
 from pytorch_lightning.loggers import TensorBoardLogger
 from smts.preprocessor import Preprocessor
-from smts.utils import expand_config_string_syntax
+from smts.utils import update_config_from_cli_args, update_config_from_path
 from tqdm import tqdm
 
 from dfaligner.config import CONFIGS, DFAlignerConfig
@@ -66,24 +64,12 @@ def train(
     accelerator: str = typer.Option("auto"),
     devices: str = typer.Option("auto"),
     strategy: str = typer.Option(None),
-    config: List[str] = typer.Option(None),
+    config_args: List[str] = typer.Option(None, "--config", "-c"),
     config_path: Path = typer.Option(None, exists=True, dir_okay=False, file_okay=True),
 ):
     original_config = CONFIGS[name.value]
-    if config is not None and config:
-        for update in config:
-            key, value = update.split("=")
-            logger.info(f"Updating config key '{key}' to value '{value}'")
-            original_config = original_config.update_config(
-                expand_config_string_syntax(update)
-            )
-        config = original_config
-    else:
-        config: DFAlignerConfig = original_config
-    if config_path is not None:
-        logger.info(f"Loading and updating config from '{config_path}'")
-        config_override = json.load(config_path)
-        config = config.update_config(config_override)
+    config: DFAlignerConfig = update_config_from_cli_args(config_args, original_config)
+    config = update_config_from_path(config_path, config)
     tensorboard_logger = TensorBoardLogger(**(config.training.logger.dict()))
     lr_monitor = LearningRateMonitor(logging_interval="step")
     logger.info("Starting training for alignment model.")
@@ -125,7 +111,7 @@ def extract_alignments(
     model_path: Path = typer.Option(
         default=None, exists=True, file_okay=True, dir_okay=False
     ),
-    config: List[str] = typer.Option(None),
+    config_args: List[str] = typer.Option(None, "--config", "-c"),
     config_path: Path = typer.Option(None, exists=True, dir_okay=False, file_okay=True),
     num_processes: int = typer.Option(None),
 ):
@@ -133,32 +119,15 @@ def extract_alignments(
     if num_processes is None:
         num_processes = 4
     original_config = CONFIGS[name.value]
-
-    if config is not None and config:
-        for update in config:
-            key, value = update.split("=")
-            logger.info(f"Updating config '{key}' to value '{value}'")
-            original_config = original_config.update_config(
-                expand_config_string_syntax(update)
-            )
-    else:
-        config = original_config
-    if config_path is not None:
-        logger.info(f"Loading and updating config from '{config_path}'")
-        if config_path.suffix == ".json":
-            with open(config_path, "r") as f:
-                config_override = json.load(f)
-        if config_path.suffix == ".yaml":
-            with open(config_path, "r") as f:
-                config_override = yaml.safe_load(f)
-        config: DFAlignerConfig = config.update_config(config, config_override)
+    config: DFAlignerConfig = update_config_from_cli_args(config_args, original_config)
+    config = update_config_from_path(config_path, config)
     data = AlignerDataModule(config)
     trainer = Trainer(
         accelerator=accelerator,
         devices=devices,
     )
     if model_path:
-        model = Aligner.load_from_checkpoint(model_path)
+        model = Aligner.load_from_checkpoint(model_path.absolute().as_posix())
         # TODO: check into the best way to update config from re-loaded model
         # model.update_config(config)
         model.config = config
@@ -182,7 +151,8 @@ def extract_alignments(
             item,
             tokens,
             pred,
-            method=config.training.extraction_method,
+            # ignore mypy type checking because https://github.com/pydantic/pydantic/issues/3809
+            method=config.training.extraction_method,  # type: ignore
         )
         torch.save(
             torch.tensor(durations).long(),
