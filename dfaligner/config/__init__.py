3754caa5b7e5aa0a30b2a4eef97251a9d3a33e79
Samuel Larkin <Samuel.Larkin@cnrc-nrc.gc.ca>
2023-09-26 09:06:29 -0400

    feat: Making the configs' properties relative paths.

--- dfaligner/config/__init__.py
+++ dfaligner/config/__init__.py
@@ -1,6 +1,6 @@
 from enum import Enum
 from pathlib import Path
-from typing import Optional, Union
+from typing import Any, Optional, Union
 
 from everyvoice.config.preprocessing_config import PreprocessingConfig
 from everyvoice.config.shared_types import (
@@ -8,11 +8,19 @@ from everyvoice.config.shared_types import (
     AdamWOptimizer,
     BaseTrainingConfig,
     ConfigModel,
+    _init_context_var,
+    init_context,
 )
 from everyvoice.config.text_config import TextConfig
 from everyvoice.config.utils import load_partials
 from everyvoice.utils import load_config_from_json_or_yaml_path
-from pydantic import Field, FilePath, model_validator
+from pydantic import (
+        Field,
+        FilePath,
+        FieldValidationInfo,
+        field_validator,
+        model_validator,
+        )
 
 
 class DFAlignerExtractionMethod(Enum):
@@ -35,6 +43,7 @@ class DFAlignerTrainingConfig(BaseTrainingConfig):
 
 
 class DFAlignerConfig(ConfigModel):
+    # TODO FastSpeech2Config and DFAlignerConfig are almost identical.
     model: DFAlignerModelConfig = Field(default_factory=DFAlignerModelConfig)
     path_to_model_config_file: Optional[FilePath] = None
 
@@ -47,12 +56,25 @@ class DFAlignerConfig(ConfigModel):
     text: TextConfig = Field(default_factory=TextConfig)
     path_to_text_config_file: Optional[FilePath] = None
 
+    def __init__(__pydantic_self__, **data: Any) -> None:
+        __pydantic_self__.__pydantic_validator__.validate_python(
+            data,
+            self_instance=__pydantic_self__,
+            context=_init_context_var.get(),
+        )
+
     @model_validator(mode="before")
-    def load_partials(self):
-        return load_partials(self, ["model", "training", "preprocessing", "text"])
+    def load_partials(self, info: FieldValidationInfo):
+        config_path = info.context.get("config_path", None) if info.context is not None else None
+        return load_partials(self,
+                ("model", "training", "preprocessing", "text"),
+                config_path=config_path,
+                )
 
     @staticmethod
     def load_config_from_path(path: Path) -> "DFAlignerConfig":
         """Load a config from a path"""
         config = load_config_from_json_or_yaml_path(path)
-        return DFAlignerConfig(**config)
+        with init_context({'config_path': path}):
+            config = DFAlignerConfig(**config)
+        return config
